4a00fe3c2a90b1e143b64ca1a1bacb9e95a230b1
Isaias <isaiascandido92@outlook.com>
2022-12-21 22:25:38 -0300

    Início da implmentação das funções JS DOM para renderizar os cards

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,14 @@
-// FAZER A RENDERIZAÇÃO COM forEach PRIMEIRO
+function renderPokeSection(pokemons){
+
+    const pkemonList = document.querySelector('.container--list') 
+
+    pokemons.forEach(pokemon => {
+        
+        const poke = createPokeSection(pokemon)
+        
+        pkemonList.appendChild(poke)
+    })
+}
+
+
+// function createPokeSection (pokemon){}
