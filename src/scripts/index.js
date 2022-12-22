91e1e2e3abf2252c2cab7504c68593d53206168f
Isaias <isaiascandido92@outlook.com>
2022-12-22 13:47:47 -0300

    Começo das funções para consumir a API e renderizar os cards

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,31 @@
-function renderPokeSection(pokemons){
+async function renderPokeSection(pokemons){
 
     const pkemonList = document.querySelector('.container--list') 
 
+    // pkemonList.innerHTML = ''
+
     pokemons.forEach(pokemon => {
         
-        const poke = createPokeSection(pokemon)
+        const poke = //createPokeSection(pokemon)
         
         pkemonList.appendChild(poke)
     })
 }
 
 
-// function createPokeSection (pokemon){}
+function createPokeSection (pokemon){
+
+    const listPoke = document.createElement('li')
+    const liDiv = document.createElement('div')   
+    
+    const numberOnPokedex = pokemon
+    const image = document.createElement('img')
+    image.src = 
+    image.alt = `${pokemon.name}`
+    liDiv.appendChild(image)
+
+    const text = document.createElement('h2')
+    text.innerText = `${pokemon.name}`   
+
+    // listPoke.
+}
